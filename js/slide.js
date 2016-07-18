c706f13f04696580c08915fc37d8f6c9e263c8c9
21Geo <mma@21Geo.com>
2016-07-18 14:22:51 -0600

    V0.2 on July 18, 2016

--- js/slide.js
+++ js/slide.js
@@ -1,49 +1,40 @@
 function initsunny()
-{   
+{ 
+
 	if($( window ).width() <= 640)
-	{		
-			
-		$('#leftControl').css("display", "block");	
-		$('#leftControl').css("height", "30");
-		$('#rightControl').css("display", "block");	
-		$('#rightControl').css("height", "30");
-		$("#head_logo").attr("src","img/logo72.jpg");		
+	{
+		$("#head_logo").attr("src","img/logo_small.jpg");		
 	}
 	else
-	{
-		$('#leftControl').css("display", "block");	
-		$('#leftControl').css("height", "60");
-		$('#rightControl').css("display", "block");	
-		$('#rightControl').css("height", "60");	
+	{	
 		$("#head_logo").attr("src","img/logo.jpg");	
 	}
-	$('#div_container').css("height", $('#s1_full').height());
-	$('#div_carousel').css("height", $('#s1_full').height());
-	$('#div_inner').css("height", $('#s1_full').height());
+	if ($('#s1_full').height() > 0)
+	{
+		$('#div_container').css("height", $('#s1_full').height());
+		$('#div_carousel').css("height", $('#s1_full').height());
+		$('#div_inner').css("height", $('#s1_full').height());
+	}
 }	
 
 
 window.onresize = function(event) {
 	if($( window ).width() <= 640)
-	{		
-			
-		$('#leftControl').css("display", "block");	
-		$('#leftControl').css("height", "30");
-		$('#rightControl').css("display", "block");	
-		$('#rightControl').css("height", "30");			
-		$("#head_logo").attr("src","img/logo72.jpg");			
+	{	
+		
+		$("#head_logo").attr("src","img/logo_small.jpg");			
 	}
 	else
-	{
-		$('#leftControl').css("display", "block");	
-		$('#leftControl').css("height", "60");
-		$('#rightControl').css("display", "block");	
-		$('#rightControl').css("height", "60");	
+	{		
 		$("#head_logo").attr("src","img/logo.jpg");	
 	}
-	$('#div_container').css("height", $('#s1_full').height());
-	$('#div_carousel').css("height", $('#s1_full').height());
-	$('#div_inner').css("height", $('#s1_full').height());
+	if ($('#s1_full').height() > 0)
+	{
+		$('#div_container').css("height", $('#s1_full').height());
+		$('#div_carousel').css("height", $('#s1_full').height());
+		$('#div_inner').css("height", $('#s1_full').height());
+	}
+	
 	
 };
 	
